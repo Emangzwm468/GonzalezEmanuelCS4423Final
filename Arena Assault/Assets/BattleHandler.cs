17b57e4c94641a0b658be4a6ae8f10b6c6c2f007
Emanuel Gonzalez <emanuelg2000@gmail.com>
2024-03-25 16:06:11 -0500

    Game update
    Sprites added

--- Arena Assault/Assets/BattleHandler.cs
+++ Arena Assault/Assets/BattleHandler.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum StateofBattle {BEGIN, PLAYERTURN, ENEMYTURN, WIN, LOST }
 
 public class BattleHandler : MonoBehaviour
 {
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    //Unit playerCharacter;
+    //Unit enemyCharacter;
+
+    public StateofBattle state;
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = StateofBattle.BEGIN;
+        BattleSetup();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BattleSetup()
     {
-        
+         Instantiate(playerPrefab);
+        //playerCharacter = playerSTART.GetComponent<Unit>();
+         Instantiate(enemyPrefab);
+        //enemyCharacter = enemySTART.GetComponent<Unit>();
     }
+
 }
